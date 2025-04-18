f8e264ebf4cd7758dbce34bad6f308e9db1c76ed
SherbekNizomiddinov <sherbekn068@gmail.com>
2025-04-18 08:30:10 +0500

    JavaScript

---
+++ script.js
@@ -0,0 +1,99 @@
+// 1   
+    // var hours = prompt("Qancha soatni minutini ko'rmoqchisiz !?");
+    // var minut = hours * 60;
+    // alert(hours + " soat " + minut + " minutga teng");
+    
+// 2   
+    // var ism = prompt("Ismingiz nima !? ");
+    // var yosh = prompt("Yoshingiz nechada !? ");
+    // alert("Salom " + ism + " yoshingiz " + yosh + "da ekan tanishganimdan hursandman.");
+
+// 3
+    // var ism = prompt("Ismingizni kiriting:");
+    // if (ism === "") {
+    //     alert("Iltimos, ismingizni kiriting!");
+    //     ism = prompt("Ismingizni kiriting:");
+    // }
+    
+    // // Yosh so'rash
+    // var yosh = prompt("Yoshingizni kiriting:");
+    // if (yosh === "" || isNaN(yosh)) {
+    //     alert("Iltimos bir bir son kiriting!");
+    //     yosh = prompt("Yoshingizni kiriting:");
+    // }
+    
+    // var talaba = prompt("Talabamisiz? (Xa/Yo'q)");
+    // var isStudent;
+    // if (talaba === "Xa") {
+    //     isStudent = true;
+    // } else if (talaba === "Yo'q") {
+    //     isStudent = false;
+    // } else {
+    //     alert("Iltimos, biror matn kiriting faqat (Xa && Yo'q)");
+    //     talaba = prompt("Talabamisiz? (Xa && Yo'q)");
+    //     if (talaba === "Xa") {
+    //         isStudent = true;
+    //     } else {
+    //         isStudent = false;
+    //     }
+    // }
+    // alert("Malumot uchun raxmat hayr salomat bo'ling 😊")
+    
+    // console.log("Ism: " + ism);
+    // console.log("Yosh: " + yosh);
+    // console.log("Talaba: " + isStudent);
+
+// 4
+    // var number_one = Number(prompt("A son kiriting - "));
+    // var number_two = Number(prompt("B son kiriting - "));
+    // alert(`Yigindi: ${number_one+number_two}ga teng`)
+
+// 5
+    // var number_one = Number(prompt("1-sonni kiriting - "));
+    // var number_two = Number(prompt("2-sonni kiriting - "));
+    // var number_three = Number(prompt("3-sonni kiriting - "));
+
+    // result = console.log(number_one + number_two + number_three) / 3;
+
+// 6
+    // var number_one = Number(prompt("1-sonni kiriting - "));
+    // var number_two = Number(prompt("2-sonni kiriting - "));
+
+    // result = console.log(number_one * number_two);
+
+// 7
+    // var number_one = Number(prompt("1-sonni kiriting - "));
+    // var number_two = Number(prompt("2-sonni kiriting - "));
+
+    // result = console.log(number_one / number_two);
+
+// 8
+    // var number_one = Number(prompt("1-sonni kiriting - "));
+    // var number_two = Number(prompt("2-sonni kiriting - "));
+
+    // result_one = number_one * number_one;
+    // result_two = number_two * number_two;
+
+    // total = result_one + result_two;
+
+    // console.log("Kvadratlar yig'indisi:",total);
+
+// 9
+    // var number_one = Number(prompt("1-sonni kiriting - "));
+    // var number_two = Number(prompt("2-sonni kiriting - "));
+
+    // result_one = number_one * number_one * number_one;
+    // result_two = number_two * number_two * number_two;
+
+    // total = result_one + result_two;
+    // console.log("Kublarning yig'indisi:",total);
+
+// 10
+    // var number_one = Number(prompt("1-sonni kiriting - "));
+    // var number_two = Number(prompt("2-sonni kiriting - "));
+    
+    // result_one = number_one * number_one * number_one;
+    // result_two = number_two * number_two * number_two;
+    
+    // total = result_one * result_two;
+    // console.log("Kublarning ko'paytmasi:",total);
